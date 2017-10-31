548908ba022be3f1baf57256fdb8d3debd126169
Johnny Mast <mastjohnny@gmail.com>
2017-10-31 22:43:58 +0100

    Added Google analytics.

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -24,8 +24,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 
-    @if (setting('site.google_analytics_tracking_id'))
-    <!-- Global site tag (gtag.js) - Google Analytics -->
+    @if (setting('site.google_analytics_tracking_id'))<!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109078007-1"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
