a7e0879e162117026d85f2aa8aa44dba0f3c21d8
Johnny Mast <mastjohnny@gmail.com>
2017-11-01 21:27:46 +0100

    Fixed css of pages.

--- resources/views/pages/view.blade.php
+++ resources/views/pages/view.blade.php
@@ -3,19 +3,15 @@
 @section('content')
 
 
-    <div class="column is-8 is-offset-2">
+    <div class="column is-8 is-offset-2 is-page">
         <section class="articles">
 
             <!-- START PROMO BLOCK -->
             <section class="hero is-dark">
                 <div class="hero-body">
                     <div class="container">
-                        <p class="title">
-                            {{ $page->title }}
-                        </p>
-                        <div class="subtitle">
-                            {{ Carbon\Carbon::parse($page->created_at)->format('d M Y i') }}
-                        </div>
+                        <p class="title">{{ $page->title }}</p>
+                        <div class="subtitle">{{ Carbon\Carbon::parse($page->created_at)->format('d M Y i') }}</div>
                     </div>
                 </div>
             </section>
@@ -24,9 +20,7 @@
             <!-- START ARTICLE -->
             <div class="card article">
                 <div class="card-content">
-                    <div class="content article-body">
-                        {!! $page->body !!}
-                    </div>
+                    <div class="content article-body">{!! $page->body !!}</div>
                 </div>
             </div>
             <!-- END ARTICLE -->
